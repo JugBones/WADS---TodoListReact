bd65e7fefdecef7804baab830d59475dcd1f05d7
JugBones <christopher.tjiandra@binus.ac.id>
2023-03-09 20:14:54 +0700

    coding

---
+++ alextodo-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>My Todo's List</h1>
+      <h6> Name : Christopher Alexander Tjiandra</h6>
+      <h6> ID : 2502019230</h6>
+    </div>
+  )
+}
+
+export default Header
